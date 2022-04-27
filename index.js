3791b487117e49b74bd9e64644af6909554f5c0d
ajormoses <ajormoses@gmail.com>
2022-04-27 17:51:23 +0100

    committed

--- index.js
+++ index.js
@@ -13,35 +13,35 @@ btn.addEventListener("click", (e) => {
   if (firstName.value.trim() == "") {
     error(firstName, "first name can not be empty");
   } else {
-    success(firstName, "");
+    success(firstName);
   }
 
   //   last name validation
   if (lastName.value.trim() == "") {
     error(lastName, "last name can not be empty");
   } else {
-    success(lastName, "");
+    success(lastName);
   }
 
   //   Password validation
   if (password.value.trim() == "") {
     error(password, "Password can not be empty");
   } else {
-    success(password, "");
+    success(password);
   }
 
   //   Email validation
   if (email.value.trim() == "") {
-    err(email, "looks like this is not an email");
+    error(email, "looks like this is not an email");
   } else {
-    success(email, "");
+    success(email);
   }
 
   //  Email pattern validation
   if (email.value.match(pattern)) {
-    success(email, "");
+    success(email);
   } else {
-    err(email, "looks like this is not an email");
+    error(email, "looks like this is not an email");
   }
 });
 
@@ -57,19 +57,13 @@ function error(element, msg) {
   p.style.color = "hsl(0, 100%, 74%)";
 }
 
-function success(element, msg) {
+function success(element) {
   element.style.border = "3px hsl(154, 59%, 51%) solid";
-}
-
-function err(element, msg) {
-  element.style.border = "3px hsl(0, 100%, 74%) solid";
   const parent = element.parentElement;
   const p = parent.querySelector("p");
   const x = parent.querySelector("img");
   const z = parent.querySelector("input");
-  const y = (z.placeholder = "email@example/com");
-  x.style.display = "block";
-  p.innerHTML = msg;
-  p.style.color = "hsl(0, 100%, 74%)";
-  z.style.color = "hsl(0, 100%, 74%)";
+  z.placeholder = "";
+  x.style.display = "none";
+  p.textContent = "";
 }
